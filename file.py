d3ae14695c89e23ef0f4f4f46056f43b801ea284
nabin <57608099+viper143@users.noreply.github.com>
2020-02-12 12:27:38 +0545

    Delete file.py

--- file.py
+++
@@ -1,6 +0,0 @@
-
-labEx = int(input("Enter lab ex"))
-questions = int(input("number of questions"))
-
-for i in range(questions + 1):
-    f = open("lab--" + str(labEx) + "-question_"+ str(i + 1) + ".py", "w")
